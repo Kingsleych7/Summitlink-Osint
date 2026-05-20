7cfdce9be283b59df132fc331d0aea5132edb43c
Kingsley <you@example.com>
2026-05-20 20:40:05 +0100

    fix duplicate sqlalchemy instance

--- models.py
+++ models.py
@@ -1,8 +1,7 @@
-from flask_sqlalchemy import SQLAlchemy
+from extensions import db
 from flask_login import UserMixin
 from datetime import datetime
 
-db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
